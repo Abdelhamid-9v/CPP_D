aa813262d0250e5068357036f057c4ad6d41628e
Abdelhamid <elhaddadi.abdo.2003@gmail.com>
2026-04-15 13:07:06 +0100

    ex01

---
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,132 @@
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {
+    std::ifstream data("data.csv");
+    std::string line;
+    std::cout << "sassssssssssssssssssssssssssss]\n";
+    if (data.is_open()) {
+        std::getline(data, line);
+        while (std::getline(data, line)) {
+            size_t a = line.find(',');
+            std::string date;
+            std::string str_price;
+            
+            if (a != std::string::npos) {
+                date = line.substr(0, a);
+                str_price = line.substr(a + 1);
+                
+                if (date.length() != 10)
+                    continue;
+                    
+                double price = std::strtod(str_price.c_str(), NULL);
+                holder[date] = price;
+            }
+        }
+    } else {
+        std::cout << "Error: could not open file.\n";
+    }
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) {
+    *this = other;
+}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& o) {
+    if (this != &o) {
+        this->holder = o.holder;
+    }
+    return *this;
+}
+
+BitcoinExchange::~BitcoinExchange() {}
+
+int BitcoinExchange::validate_date(std::string& date) {
+    if(date.length() != 10)
+        return 0;
+    if(date[4] != '-' || date[7] != '-')
+        return 0;
+    for (int i=0; i < 10 ;i++) {
+        if (i == 4 || i == 7)
+            continue;
+        if (!std::isdigit(date[i]))
+            return 0;
+    }
+    
+    int year = std::atoi(date.substr(0, 4).c_str());
+    int month = std::atoi(date.substr(5, 2).c_str());
+    int day = std::atoi(date.substr(8, 2).c_str());
+
+    if (month < 1 || month > 12)
+        return 0;
+    if (day < 1 || day > 31)
+        return 0;
+    if (month == 4 || month == 6 || month == 9 || month == 11) {
+        if (day > 30)
+            return 0;
+    } else if (month == 2) {
+        if (year % 400 == 0 && day > 29)
+            return 0;
+        else if (year % 100 == 0 && year % 400 != 0 && day > 28)
+            return 0;
+        else if (year % 4 == 0 && year % 100 != 0 && day > 29)
+            return 0;
+        else if (year % 4 != 0 && day > 28)
+            return 0;
+    }
+    return 1;
+}
+
+int BitcoinExchange::validate_value(std::string str_price, double& value) {
+    value = std::strtod(str_price.c_str(), NULL);
+    if (value < 0) {
+        std::cout << "Error: not a positive number.\n";
+        return 0;
+    }
+    if (value > 1000) {
+        std::cout << "Error: too large a number.\n";
+        return 0;
+    }
+    return 1;
+}
+
+void BitcoinExchange::process_input(char *filename) {
+    std::ifstream input(filename);
+    std::string line;
+    
+    if(input.is_open()) {
+        std::getline(input, line);
+        while (std::getline(input, line)) {
+            size_t a = line.find('|');
+            std::string date;
+            std::string str_price;
+
+            if (a != std::string::npos) {
+                date = line.substr(0, a - 1);
+                str_price = line.substr(a + 1);
+
+                if (validate_date(date)) {
+                    double val;
+                    if(validate_value(str_price, val)) {
+                        std::map<std::string, double>::iterator it = holder.lower_bound(date);
+                        
+                        if (it == holder.begin() && it->first != date) {
+                            std::cout << "Error: bad input => " << date << std::endl;
+                            continue;
+                        }
+                        if (it->first != date)
+                            --it;
+                            
+                        double result = val * it->second;
+                        std::cout << date << " => " << val << " = " << result << std::endl;
+                    }
+                } else {
+                    std::cout << "Error: bad input => " << date << std::endl;
+                }
+            } else {
+                std::cout << "Error: bad input => " << line << std::endl;
+            }
+        }
+    } else {
+        std::cout << "Error: could not open file.\n";
+    }
+}
---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,26 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <cstdlib>
+#include <cctype>
+
+class BitcoinExchange {
+private:
+    std::map<std::string, double> holder;
+
+public:
+    BitcoinExchange();
+    BitcoinExchange(const BitcoinExchange& other);
+    BitcoinExchange& operator=(const BitcoinExchange& rhs);
+    ~BitcoinExchange();
+
+    int validate_date(std::string& date);
+    int validate_value(std::string str_price, double& value);
+    void process_input(char *filename);
+};
+
+#endif
