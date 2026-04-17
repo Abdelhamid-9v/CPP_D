5a20a6d2cfa98494d2c1fc378379a80153a9e993
Abdelhamid El Haddadi <abel-had@e2r3p9.1337.ma>
2026-04-17 16:39:25 +0100

    sort_deque

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <cctype>
 #include <algorithm>
-
+#include <ctime>
 
 
 int is_number(const std::string str)
@@ -57,11 +57,7 @@ void sort_vec(std::vector<int>& v)
         else
             pair.push_back(std::make_pair(v[i+1], v[i]));
     }
-    // for (size_t i = 0; i < pair.size(); i++) {
-    // std::cout << "Pair: Winner = " << pair[i].first 
-    //           << ", Loser = " << pair[i].second << std::endl;
-    // }
-
+ 
 
     std::vector<int> main_v;
     for (size_t i = 0; i < pair.size(); i++)
@@ -101,7 +97,7 @@ void sort_vec(std::vector<int>& v)
         {
             int current_loser = pend_v[i];
             std::vector<int>::iterator win_it;
-            if (!p && i == (int)pend_v.size() - 1)
+            if (!p && i == static_cast<int>(pend_v.size()) - 1)
                 win_it = main_v.end();
             else
             {
@@ -124,6 +120,91 @@ void sort_vec(std::vector<int>& v)
     }
     v = main_v;
 }
+
+
+
+
+
+void sort_deque(std::deque<int>& dq)
+{
+    if(dq.size() <= 1)
+        return;
+    std::deque<std::pair<int, int>> pair;
+    bool  p = true;
+    if(dq.size()%2 != 0)
+        p = false;
+    for (int i = 0; i < dq.size() - 1; i = i + 2)
+    {
+        if (dq[i] > dq[i + 1])
+            pair.push_back(std::make_pair(dq[i], dq[i+1]));
+        else
+            pair.push_back(std::make_pair(dq[i+1], dq[i]));
+    }
+ 
+
+    std::deque<int> main_dq;
+    for (size_t i = 0; i < pair.size(); i++)
+    {
+        main_dq.push_back(pair[i].first);
+    }
+    sort_deque(main_dq);
+
+    std::deque<int> pend_dq;
+
+    for (size_t i = 0; i < main_dq.size(); i++)
+    {
+        for (size_t j = 0; j < pair.size(); j++)
+        {
+            if (main_dq[i] == pair[j].first)
+            {
+                pend_dq.push_back(pair[j].second);
+                break;
+            }
+        }
+    }
+    if (!p)
+        pend_dq.push_back(dq[dq.size() - 1]);
+    
+    main_dq.insert(main_dq.begin(), pend_dq[0]);
+
+    int jacob_indx = 3;
+    int last_inserted_indx = 1;
+    
+    int i = main_dq.size() - 1;
+    while (++i < dq.size())
+    {
+        int curr_jac = get_jacobsthal(jacob_indx);
+        if (curr_jac > pend_dq.size())
+            curr_jac = pend_dq.size();
+       for (int i = curr_jac - 1; i >= last_inserted_indx ; i--)
+        {
+            int current_loser = pend_dq[i];
+            std::deque<int>::iterator win_it;
+            if (!p && i == static_cast<int>(pend_dq.size()) - 1)
+                win_it = main_dq.end();
+            else
+            {
+                int winner_value = -1;
+                for (int j = 0; j < pair.size(); j++)
+                {
+                    if (pair[j].second == current_loser)
+                    {
+                        winner_value = pair[j].first;
+                        break;
+                    }
+                }
+                win_it = std::lower_bound(main_dq.begin(), main_dq.end(), winner_value);
+            }
+            std::deque<int>::iterator it = std::lower_bound(main_dq.begin(), win_it, current_loser);
+            main_dq.insert(it, current_loser);
+        }
+        last_inserted_indx = curr_jac;
+        jacob_indx++;
+    }
+    dq = main_dq;
+}
+
+
 
 
 int main (int ac , char **av)
@@ -155,11 +236,47 @@ int main (int ac , char **av)
         vec_base.push_back(nb);
         deq_base.push_back(nb);
     }
-    sort_vec(vec_base);
-
+    std::cout << "Before: ";
     for(int i = 0; i < vec_base.size();i++)
-    {
         std::cout << vec_base[i] << " ";
-    }
+    std::cout << std::endl;
+
+    clock_t start_vec = clock();
+    sort_vec(vec_base);
+    clock_t end_vec = clock();
+    
+    std::cout << "After: ";
+    for(int i = 0; i < vec_base.size();i++)
+        std::cout << vec_base[i] << " ";
+    std::cout << std::endl;
+
+
+    double time_vec = static_cast<double>(end_vec - start_vec);
+
+
+
+    
+    
+    
+    /////////////////////////////////////////////////////////////////////////
+    
+    
+    clock_t start_deq = clock();
+    sort_deque(deq_base);
+    clock_t end_deq = clock();
+
+    
+    double time_deq = static_cast<double>(end_deq - start_deq);
+    
+    std::cout << "Time to process a range of " << vec_base.size() 
+              << " elements with std::vector : " << time_vec << " us" << std::endl;
+
+
+    std::cout << "Time to process a range of " << deq_base.size() 
+              << " elements with std::deque : " << time_deq << " us" << std::endl;
+
+
+
+
     return 0;
 }
